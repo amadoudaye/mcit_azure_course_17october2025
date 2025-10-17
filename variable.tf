c64dd7240b3c9c66d76e5fd51955ae2538f420a6
Sami Islam <islam.samipkr@gmail.com>
2025-10-17 19:32:12 -0400

    Add new variables for replication type and environment

--- variable.tf
+++ variable.tf
@@ -30,3 +30,11 @@ variable "account_tier"{
   type=string
   default="Standard"
 }
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "environment"{
+  type=string
+  default="production"
+}

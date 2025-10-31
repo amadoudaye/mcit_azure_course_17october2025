a05f79c897c9319f0f52e672132d858edf43132f
amadoudaye <amadoudaye@gmail.com>
2025-10-31 18:45:04 -0400

    Fix duplicate required_version in provider.tf
    Removed duplicate required_version declaration.

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 4.0"   # 👈 forces Terraform Cloud to use the latest v4.x provider
     }
   }
-  required_version = ">= 1.6.0"
 }
 
 provider "azurerm" {

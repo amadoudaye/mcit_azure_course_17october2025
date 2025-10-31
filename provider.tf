fb4ad201bb9a02104e6ff981cb37dfbd1fcb4332
amadoudaye <amadoudaye@gmail.com>
2025-10-31 18:55:10 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,17 @@
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"   # 👈 forces Terraform Cloud to use the latest v4.x provider
+      version = ">=3.70.0"   # 👈 This version is for azurem, not terraform version
     }
   }
+required_version=">=1.4.0" #this version is for terraform version, not azurem
 }
 
 provider "azurerm" {
   features {}
+subscription_id=var.subscription_id
+client_id=var.client_id
+client_secret=var.client_secret
+tenant_id=var.tenant_id
 }

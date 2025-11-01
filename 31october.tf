83722241a5884afb2dc30b4094690ff4505d2224
amadoudaye <amadoudaye@gmail.com>
2025-10-31 20:20:13 -0400

    Update 31october.tf

--- 31october.tf
+++ 31october.tf
@@ -1,37 +1,43 @@
-# works with code of 29 october.tf
+provider "azurerm" {
+  features {}
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg_new" {
+  name     = "rg-terraform-demo"
+  location = "Canada Central"
+}
+
+# Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "my-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg_new.location
   resource_group_name = azurerm_resource_group.rg_new.name
 }
+
+# Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "my-subnet"
   resource_group_name  = azurerm_resource_group.rg_new.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-resource "azurerm_network_interface" "nic" {
-  name                = "my-nic"
-  location            = azurerm_resource_group.rg_new.location
-  resource_group_name = azurerm_resource_group.rg_new.name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
-  }
-}
+
+# Create Public IP
 resource "azurerm_public_ip" "pip" {
   name                = "my-public-ip"
   location            = azurerm_resource_group.rg_new.location
   resource_group_name = azurerm_resource_group.rg_new.name
   allocation_method   = "Dynamic"
 }
+
+# Create Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   name                = "my-nsg"
   location            = azurerm_resource_group.rg_new.location
   resource_group_name = azurerm_resource_group.rg_new.name
+
   security_rule {
     name                       = "AllowSSH"
     priority                   = 1001
@@ -44,22 +50,43 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix  = "*"
   }
 }
-resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+
+# Create Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "my-nic"
+  location            = azurerm_resource_group.rg_new.location
+  resource_group_name = azurerm_resource_group.rg_new.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Associate NSG with NIC
+resource "azurerm_network_interface_security_group_association" "assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+# Create Linux VM
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "myvm01"
   resource_group_name = azurerm_resource_group.rg_new.name
   location            = azurerm_resource_group.rg_new.location
   size                = "Standard_B2s"
   admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
   network_interface_ids = [azurerm_network_interface.nic.id]
-  admin_password = var.admin_password
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
